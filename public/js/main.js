596ac6bff99171d041f53f89cad6a56db86fec0f
germanio7 <german.e.escobar@gmail.com>
2019-06-20 11:27:29 -0300

    mezclando

--- public/js/main.js
+++ public/js/main.js
@@ -58817,7 +58817,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\temporal\Gepetto-Point-Of-Sale\resources\js\main.js */"./resources/js/main.js");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\Gepetto-Point-Of-Sale\resources\js\main.js */"./resources/js/main.js");
 
 
 /***/ })
